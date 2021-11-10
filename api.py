43134b52ec93589a44d1a5f775b3a36403f1b2c0
odysseus <odysseus@Ithaca-MS-7C91>
2021-11-09 22:22:28 -0600

    Create function to filter products list based on ingredients

--- api.py
+++ api.py
@@ -6,18 +6,32 @@ app = Flask(__name__)
 api = Api(app)
 #api.add_resource(product, '/product')
 
-products = {"Clorox": {"name": "clorox", "ingredients": ["sodium hypochlorite", "water", "sodium chloride"]},
-            "Windex": {"name": "windex", "ingredients": ["water", "ammonium hydroxide"]}}
+products = {"clorox": {"name": "clorox", "ingredients": ["sodium", "water", "chlorine"]},
+            "windex": {"name": "windex", "ingredients": ["water", "ammonium"]}}
 
 print("Dictionary:", products)
 
-@app.get('/products')
-def get_all():
-    return {'products':list(products.values())}
+@app.get('/all_products')
+def list_all_products():
+    return{"products":list(products.values())}
 
-# @app.route('/products/<product_name>')
-# def get(product_name):
-#     return products[product_name], 200
+@app.get('/products')
+def list_products():
+    ingredient = request.args.get('ingredient')
+    if ingredient:
+        qualifying_data = list(
+            filter(
+                lambda p1: str(ingredient) in p1['ingredients'],
+                products.values()
+            )
+        )
+        return {'products':qualifying_data}
+    else:
+        return{"products":list(products.values())}
+
+@app.route('/products/<product_name>')
+def get(product_name):
+    return products[product_name]
 
 if __name__ == '__main__':
     app.run()
