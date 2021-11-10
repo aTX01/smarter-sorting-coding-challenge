3c4930f1f8817f25ac7dec22dfa6d4f54fd1e32a
odysseus <odysseus@Ithaca-MS-7C91>
2021-11-09 20:10:06 -0600

    Instantiate flask application

--- api.py
+++ api.py
@@ -11,7 +11,7 @@ from flask.cli import with_appcontext
 def get_database():
     if 'db' not in g:
         g.db = sqlite3.connect(
-            current_app.config['DATABASE']
+            current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLITYPES
         )
         g.db.row_factory = sqlite3.Row
@@ -23,6 +23,23 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
 
 class product(Resource):
     def __init__(self):
