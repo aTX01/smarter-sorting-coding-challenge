77073506a712d41af3d958a3f5bea9557d10d900
odysseus <odysseus@Ithaca-MS-7C91>
2021-11-10 20:51:58 -0600

    Instantiate database within api app and add helper functions

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, g
 from flask_restful import Resource, Api, reqparse
+import sqlite3
 
 
 app = Flask(__name__)
@@ -11,9 +12,46 @@ products = {"clorox": {"name": "clorox", "ingredients": ["sodium", "water", "chl
 
 print("Dictionary:", products)
 
+connection = sqlite3.connect('products.db')
+cursor = connection.cursor()
+command1 = """CREATE TABLE IF NOT EXISTS
+products(name TEXT PRIMARY KEY, ingredients TEXT)"""
+cursor.execute(command1)
+DATABASE='./products.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+
+    def convert_to_dicts(cursor, row):
+        return dict((cursor.description[i][0], val)
+            for i, val in enumerate(row))
+
+    db.row_factory = convert_to_dicts
+
+    return db
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    print("rv", rv)
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 @app.get('/all_products')
 def list_all_products():
-    return{"products":list(products.values())}
+    cur = get_db().cursor()
+    database = get_db()
+    print(query_db('SELECT * FROM products'))
+    #return query_db('select * from products')
+    # return{"products":list(products.values())}
 
 @app.get('/products')
 def list_products():
